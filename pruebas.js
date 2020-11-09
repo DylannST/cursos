5f9b7d49a321c46c6aa18b454f128bf47c1b25d6
DylannST <dguarnizoserrato@gmail.com>
2020-11-09 14:47:26 -0500

    se agrega los arrays

--- pruebas.js
+++ pruebas.js
@@ -7,3 +7,9 @@ condition ? true : false;
 var palabra1 = "verde";
 var palabra2 = "azul";
 var guardar = palabra1 === "azul " ? "es azul" : "no es azul";
+
+var lista =["papa","melon", "zanahoria"]
+//muestra la posicion
+lista.indexOf("melon")
+//agrega al principio del array
+lista.unshift("gelatina")
