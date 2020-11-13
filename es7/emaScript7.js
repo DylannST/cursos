d34916d9771240c925202cf6eb457b0c57ab684a
DylannST <dguarnizoserrato@gmail.com>
2020-11-13 09:47:53 -0500

    se agrego las funciones nuevas de emaScript7

---
+++ es7/emaScript7.js
@@ -0,0 +1,15 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+
+if (numeros.includes(8)) {
+  //sirve para saber si incluye el numero en este caso o tambien coon String
+  console.log("si se encuentra el numero 4");
+} else {
+  console.log("no se encontro el numero");
+}
+
+//----------------------------------------------
+
+let base = 7;
+let exponenete = 2;
+let resultado = base ** exponenete; //para usar potencias
+console.log(resultado);
