9b13f86757c1760b0fa7ea40e8729551b469a72a
DylannST <dguarnizoserrato@gmail.com>
2020-11-26 16:34:56 -0500

    se agrego como se hereda sin azucar sintactico

--- prototype.js
+++ prototype.js
@@ -11,6 +11,24 @@ function operar(nombre, a, b) {
 
 const numero1 = new operar("el primero", 2, 4);
 console.log(numero1.name);
+
 console.log(numero1.restar());
 
 //Herencia prototipal-----------------------------------------------
+function triangulo(nombre, a, b, puntas) {
+  this.super = operar;
+  this.super(nombre, a, b);
+  this.puntas = puntas;
+}
+
+//aqui se hace la herencia
+triangulo.prototype = new operar();
+const valor = new triangulo("triangulo", 5, 6, 3);
+console.log(valor.name);
+
+//reescribir-------------------------------------------------
+triangulo.prototype.restar = function () {
+  console.log(this.a * this.b);
+};
+
+console.log(valor.restar());
